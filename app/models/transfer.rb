5046dcf70a1f1f1b94a7c4c9a8382cd19bf09981
pancake-lover <anankhee@gmail.com>
2016-12-11 23:00:09 +0100

    add basic validations for transfers and users

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer < ApplicationRecord
   belongs_to :user
+
+  validates :account_number_from, presence: true, length: { is: 18 }
+  validates :account_number_to, presence: true, length: { is: 18 }
+  validates :amount_penies, numericality: { only_integer: true, greater_than: 0 }
+  validates :country_code_from, presence: true, length: { is: 3 }
+  validates :country_code_to, presence: true, length: { is: 3 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :transfers
+
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :dob, presence: true
 end
