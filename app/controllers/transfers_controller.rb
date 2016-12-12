7958e8b947626d7f8411fc4845751b8aa1d72c35
pancake-lover <anankhee@gmail.com>
2016-12-12 17:14:42 +0100

    set user in create action

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,31 +1,27 @@
 class TransfersController < ApplicationController
   before_action :set_transfer, only: [:show, :update, :destroy]
+  before_action :set_user, only: :create
   before_action :authenticate_user
 
-  # GET /transfers
   def index
     @transfers = Transfer.all
 
     render json: @transfers
   end
 
-  # GET /transfers/1
   def show
     render json: @transfer
   end
 
-  # POST /transfers
   def create
     @transfer = Transfer.new(transfer_params)
-
     if @transfer.save
-      render json: @transfer, status: :created, location: @transfer
+      render json: @transfer, status: :created, location: @user
     else
       render json: @transfer.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /transfers/1
   def update
     if @transfer.update(transfer_params)
       render json: @transfer
@@ -34,7 +30,6 @@ class TransfersController < ApplicationController
     end
   end
 
-  # DELETE /transfers/1
   def destroy
     @transfer.destroy
   end
@@ -45,8 +40,14 @@ class TransfersController < ApplicationController
       @transfer = Transfer.find(params[:id])
     end
 
+    def set_user
+      @user = User.find(params[:user_id])
+      puts @user
+    end
+
     # Only allow a trusted parameter "white list" through.
     def transfer_params
-      params.require(:transfer).permit(:account_number_from, :account_number_to, :amount_pennies, :country_code_from, :country_code_to, :user_id)
+      params.require(:transfer).permit(:account_number_from, :account_number_to, :amount_pennies, :country_code_from, :country_code_to)
+      .merge(user_id: params.require(:user_id))
     end
 end
